2601136c4b98f5707dd98fcb7aab90e6b26a5533
Erin Woody <erinelizabethwoody@gmail.com>
2017-07-26 16:06:42 -0500

    second commit

--- main.js
+++ main.js
@@ -2,46 +2,80 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // Then, write and example of using the function.
 
-function max(){
-    // Your answer here
+function max(a, b) {
+    if (b > a) {
+        return b;
+    }
+    else {
+        return a;
+    }    
 }
 
+console.log(max(4, 5));
 
 // 2.
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // Then, write and example of using the function.
 
-function maxOfThree(){
-    // Your answer here
+function maxOfThree(a,b,c){
+    if ((a >= b) && (a >= c)) {
+        return a;
+    } else if ((b >= a) && (b >= c)) {
+        return b;
+    } else {
+        return c;
+    }
 }
 
+
+console.log(maxOfThree(2, 3, 1));
 
 // 3.
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // Then, write and example of using the function.
 
 function isVowel(char){
-    // Your answer here
+    if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == "u") {
+        return true;
+    } else {
+        return false;
+        
+    }
 }
 
+console.log(isVowel(1));
 
 // 4.
 // Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
 // Then, write and example of using the function.
 
+function sum(a, b) {
+    result = a + b 
+        return result;
+    }
 
+console.log(sum(5, 5));
 
 // 5.
 // Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
 // Then, write and example of using the function.
 
+function avg(a, b, c) {
+    result = (a + b + c)/3
+    return result;
+}
 
+console.log(avg(3, 4, 5));
 
 // 6.
 // Write a function called `getLength` that takes one parameter (a string) and returns the length
 // Then, write and example of using the function.
 
+function getLength(abc) {
+    return abc.length;
+}
 
+console.log(getLength("Erin"));
 
 // 7.
 // Write a function called `greaterThan` that takes two parameters
@@ -49,7 +83,15 @@ function isVowel(char){
 // Otherwise the function should return `false`.
 // Then, write and example of using the function.
 
+function greaterThan(a, b) {
+    if (a > b) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
+console.log(greaterThan(5, 8));
 
 // 8.
 // Write a function called `greet` that takes a
@@ -58,6 +100,11 @@ function isVowel(char){
 // is the parameter that was passed in.
 // Then, write and example of using the function.
 
+function greet(name) {
+    return "Hello, " + name + "!";   
+}
+
+console.log(greet("Erin"));
 
 
 // 9.
@@ -68,3 +115,7 @@ function isVowel(char){
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
 // Then, write and example of using the function.
+
+function madlib(1, 2, 3, 4) {
+    
+}
